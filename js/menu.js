6d6e9f96a6ed4df9b7d5fe5b1046cf4d094297cd
chasteivar26 <132213727+chasteivar26@users.noreply.github.com>
2023-05-13 18:50:15 +0200

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+function clicked(){
+    document.getElementById("last").style.left = "0"
+    document.getElementById("igg").style.zIndex = "0"
+    document.getElementById("igg").style.left = "30%"
+    document.getElementById("close").style.left = "30%"
+}
+function closed(){
+    
+    document.getElementById("imm").src="./icons/menu.png"
+}
+function clic(){
+    document.getElementById("last").style.left = "-500px"
+    document.getElementById("igg").style.zIndex = "-1"
+    document.getElementById("igg").style.left = "0"
+    document.getElementById("close").style.left = "0"
+}
